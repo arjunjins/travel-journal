66c877b480eaffdf1229adab3d8002869e2d9863
Arjun J <arjunjins2001@gmail.com>
2022-12-25 15:30:44 +0530

    setup links and pictures

--- data.js
+++ data.js
@@ -14,22 +14,36 @@ export default [
     id: 2,
     title: "Sydney Opera House",
     location: "Australia",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    googleMapsUrl: "https://goo.gl/maps/F5B6K4DfqwEPV8vs5",
     startDate: "27 May, 2021",
     endDate: "8 Jun, 2021",
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the Sydney Harbour,it is often regarded as one of the 20th century's most famous and distinctive buildings",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    imageUrl:
+      "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   },
   {
     id: 3,
     title: "Geirangerfjord",
     location: "Norway",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    googleMapsUrl: "https://goo.gl/maps/qWCVpjxbyRv1FF1x7",
     startDate: "1 Oct, 2021",
     endDate: "18 Nov, 2021",
     description:
-      "The Geiranger Fjord is a fjord in the Sunnm for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1587589396598-433f04c9623b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  },
+  {
+    id: 4,
+    title: "Kuala Lumpur",
+    location: "Malaysia",
+    googleMapsUrl: "https://goo.gl/maps/d5rd2KggvB2C2drU8",
+    startDate: "25 Dec, 2022",
+    endDate: "31 Dec, 2022",
+    description:
+      "Kuala Lumpur is officially the Federal Territory of Kuala Lumpur and colloquially referred to as KL, is a federal territory and the ceremonial, legislative and judicial capital city of Malaysia. It is one of the fastest growing cities in Asia",
+    imageUrl:
+      "https://images.unsplash.com/photo-1596422846543-75c6fc197f07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1464&q=80",
   },
 ];

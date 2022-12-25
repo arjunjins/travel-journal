5300bcd342f26e6f7c12013e86f9f639d7cf849c
Arjun J <arjunjins2001@gmail.com>
2022-12-25 11:29:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./assets/components/Card/Card";
+import Navbar from "./assets/components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;

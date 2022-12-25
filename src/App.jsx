9b589360c4ceb27500205f937266848968aed894
Arjun J <arjunjins2001@gmail.com>
2022-12-25 13:41:33 +0530

    setup phone error page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import Navbar from "./assets/components/Navbar/Navbar";
 function App() {
   let size = screen.width;
   if (size < 1000) {
-    return <div className="App-not-working">Please use a desktop</div>;
+    return (
+      <div className="App-not-working">
+        <h1>Please use a desktop</h1>
+      </div>
+    );
   } else if (size > 1000) {
     return (
       <div className="App">

9346a2ff9928e5f572261b4f2518651ebecf1012
Arjun J <arjunjins2001@gmail.com>
2022-12-25 12:51:16 +0530

    card half done

--- src/assets/components/Card/Card.jsx
+++ src/assets/components/Card/Card.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import "./Card.css";
+
 const Card = () => {
-  return <div className="card">Card component</div>;
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img
+          src="https://source.unsplash.com/WLxQvbMyfas"
+          alt=""
+          className="place-img"
+        />
+      </div>
+      <div className="content-container">
+        <div className="header">
+          <img
+            src="../../../../public/map.png"
+            alt="map-logo"
+            className="map-logo"
+          />
+          <h4 id="placename">JAPAN</h4>
+          <a href="">View on Google Maps</a>
+        </div>
+        <h1 className="location">Mount Fuji</h1>
+        <h3>12 Jan, 2021 - 24 Jan, 2021</h3>
+        <p>
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+          (12,380 feet). Mount Fuji is the single most popular tourist site in
+          Japan, for both Japanese and foreign tourists.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;

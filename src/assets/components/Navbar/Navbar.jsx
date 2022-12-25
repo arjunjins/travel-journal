897245e41ef459abd079d33d6700f2ef17558918
Arjun J <arjunjins2001@gmail.com>
2022-12-25 12:16:04 +0530

    navbar added

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Navbar.css";
 const Navbar = () => {
-  return <div className="navbar">Navbar component</div>;
+  return (
+    <div className="navbar">
+      <img src="./globe-navbar.png" alt="navbar-logo" className="logo" />
+      <p>my travel journal.</p>
+    </div>
+  );
 };
 
 export default Navbar;

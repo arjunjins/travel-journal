82518ae21b9e9fb90a2fceb20513a36a88e6c7ac
Arjun J <arjunjins2001@gmail.com>
2022-12-25 13:35:16 +0530

    optimised for laptop

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import Card from "./assets/components/Card/Card";
 import Navbar from "./assets/components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Card />
-    </div>
-  );
+  let size = screen.width;
+  if (size < 1000) {
+    return <div className="App-not-working">Please use a desktop</div>;
+  } else if (size > 1000) {
+    return (
+      <div className="App">
+        <Navbar />
+        <Card />
+        <Card />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/assets/components/Card/Card.jsx
+++ src/assets/components/Card/Card.jsx
@@ -22,8 +22,8 @@ const Card = () => {
           <a href="">View on Google Maps</a>
         </div>
         <h1 className="location">Mount Fuji</h1>
-        <h3>12 Jan, 2021 - 24 Jan, 2021</h3>
-        <p>
+        <h3 className="dates">12 Jan, 2021 - 24 Jan, 2021</h3>
+        <p className="desc">
           Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
           (12,380 feet). Mount Fuji is the single most popular tourist site in
           Japan, for both Japanese and foreign tourists.
